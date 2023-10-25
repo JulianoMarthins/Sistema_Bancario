14571d7ca370af6ca1d93788239a02153e3f3dea
julianomarthins <julianomarthins@yahoo.com.br>
2023-10-25 19:56:06 -0300

    Substituíção do if e else para match case.
    Implementação de todos os métodos antigos exceto os novos solicitados

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 LIMITE_SAQUE = 500  # Constante, valor não deve ser alterado
 saque_diario = 3
 
@@ -5,23 +7,56 @@ extrato = ""
 saldo = 0.0
 
 
-# Método abaixo realiza deposito na conta. Validação: Aceito valores acima de zero
+# Método realiza deposito na conta
+# Validação: Não aceito valores iguais ou inferior a zero
 def deposito(valor, saldo, extrato):
     if valor > 0:
         saldo += valor
         extrato += f"Deposito: R$ {valor:.2f}; Saldo: R$ {saldo:.2f}\n"
+        print("=-" * 30)
         print("Deposito realizado com sucesso")
         print("=-" * 30)
         return saldo, extrato
     else:
+        print("=-" * 30)
         print("Valor inválido para depósito".center(60))
         print("Em caso de duvidas, favor entrar em contato com seu gerente de conta".center(60))
         print("=-" * 30)
         return None
 
 
+# Método realiza saque da conta
+# Validação: Limite de três saques por dia
+# Validação: Limite de R$ 500,00 por saque
+def saque(valor, saldo, extrato, saque_diario):
+    if valor <= LIMITE_SAQUE:
+        if saque_diario > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}; Saldo: R$ {saldo:.2f}\n"
+            saque_diario -= 1
+            print("=-" * 30)
+            print("Saque realizado com sucesso")
+            print("=-" * 30)
+            return saldo, extrato
+        else:
+            print("=-" * 30)
+            print("Limite de saque diário excedido")
+            print("=-" * 30)
+            return None
+    else:
+        print("=-" * 30)
+        print("Valor soliticado acima do permitido")
+        print("=-" * 30)
+        return None
 
 
+# Método retorna o extrato
+# Validação: Verifica se o extrato possui lançamento para impressão
+def mostrar_extrato(extrato):
+    if extrato is None or len(extrato) == 0:
+        return "Não há movimentação neta conta".center(60)
+    else:
+        return extrato
 
 
 print()
@@ -46,89 +81,51 @@ menu = """
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 """
 
-
-
 while True:
     opcao = int(input(menu))
     print("=-" * 30)
 
     # Fechar o programa
-    if opcao == 0:
+    match opcao:
+        case 0:
+            print("Obrigado por visitar o Poa's Bank".center(60))
+            print("=-" * 30)
+            break
 
-        print("Obrigado por visitar o Poa's Bank".center(60))
-        print("=-" * 30)
+        # Realização de depósito
+        case 1:
+            while True:
+                try:
+                    print("Digite o valor a depositar: ".center(60))
+                    valor = input()
+                    valor = float(valor)
+                    break
+                except Exception as e:
+                    print("Digite um valor válido: ")
 
-        break
+            saldo, extrato = deposito(valor=valor, saldo=saldo, extrato=extrato)
 
-    # Realização de depósito
-    elif opcao == 1:
-        while True:
-            try:
-                print("Digite o valor a depositar: ".center(60))
-                valor = input()
-                valor = float(valor)
-                break
-            except Exception as e:
-                print("Digite um valor válido: ")
-
-        saldo, extrato = deposito(valor=valor, saldo=saldo, extrato=extrato)
-
-
-
-    # Realização de saque
-    elif opcao == 2:
-        if saque_diario > 0:
+        # Realização de saque
+        case 2:
             print("Digite o valor a sacar: ".center(60))
             valor = input()
             valor = float(valor)
+            saldo, extrato = saque(valor, saldo, extrato, saque_diario)
 
-            # Validação, verifica se o valor do saque está abaixo do limite por operação
-            if valor <= LIMITE_SAQUE:
-
-                # Validação, verifica se há saldo em conta para o saque
-                if valor <= saldo:
-                    saldo -= valor
-                    saque_diario -= 1
-                    print("=-" * 30)
-                    extrato += f"Saque: R$ {valor:.2f}. Saldo atual: R$ {saldo:.2f}\n"
-
-
-                else:
-                    print("Saldo insuficiente".center(60))
-                    print("=-" * 30)
-
-
-            else:
-                print("Limite diário de saque excedido, favor consultar a gerência".center(60))
-                print("=-" * 30)
-
-
-        else:
-            print("Limite de saque diários excedidos, duvidas, favor consultar a gerência".center(60))
-            print("=-" * 30)
-
-
-    # Retorna o extrato atualizado ao cliente
-    elif opcao == 3:
-
-        # Validação, nega impressão do extrato caso o mesmo esteja sem informações
-        if extrato is None or len(extrato) == 0:
-            print("Não há registros de movimentações nesta conta.".center(60))
-            print("=-" * 30)
-
-
-        else:
+        # Retorna o extrato atualizado ao cliente
+        case 3:
             print(f"Extrato:".center(60))
-            print(extrato)
+            texto = mostrar_extrato(extrato)
+            print(texto)
             print("=-" * 30)
 
+        # Cadastra novos clientes
+        case 4:
+            continue
 
-    else:
-        print("ERRO! \n Valor digitado é inválido")
+        # Cadastra nova conta
+        case 5:
+            continue
 
-
-
-
-
-
-
+        case _:
+            print("ERRO! \n Valor digitado é inválido")
